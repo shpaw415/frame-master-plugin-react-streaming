4908bfbf0806bb71490ab4d800d9b4cfbd96edbb
Justin <shpaw415@gmail.com>
2026-01-04 13:57:43 -0500

    add useRequest

--- src/utils.ts
+++ src/utils.ts
@@ -3,12 +3,14 @@ import { createContext, useContext } from "react";
 
 export const RequestContext = createContext<masterRequest | null>(null);
 
+/**
+ * Hook to access the current masterRequest in React components.
+ *
+ * - masterRequest is available only during server-side rendering.
+ * - null is returned if used outside of a request context. (client-side or outside SSR)
+ * @returns masterRequest | null
+ */
 export function useRequest() {
   const req = useContext(RequestContext);
-  if (!req) {
-    throw new Error(
-      "useRequest must be used within a RequestProvider and server-side only."
-    );
-  }
   return req;
 }
