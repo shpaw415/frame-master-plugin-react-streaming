5fe5e748472556d806c09fa0326ddb62c866c8c8
Justin <shpaw415@gmail.com>
2026-01-03 22:37:48 -0500

    first

---
+++ index.ts
@@ -0,0 +1,92 @@
+import type { FrameMasterPlugin } from "frame-master/plugin/types";
+import { name, version } from "./package.json";
+import type { JSX } from "react";
+import type { masterRequest } from "frame-master/server/request";
+import { isDev } from "frame-master/utils";
+import {
+  renderToReadableStream,
+  type RenderToReadableStreamOptions,
+} from "react-dom/server";
+
+declare module "frame-master/plugin/utils" {
+  interface CustomDirectives {
+    "use-streaming": true;
+  }
+}
+
+export type ReactStreamingPluginOptions = {
+  /**
+   * The source directory to scan for 'use-dynamic' directives.
+   */
+  baseSrc: string;
+  /**
+   * A function to parse the entry point JSX element.
+   *
+   * Useful for wrapping the page element with additional providers or layouts.
+   */
+  parseEntryPoint?: (
+    pageElement: JSX.Element,
+    request: masterRequest
+  ) => JSX.Element | Promise<JSX.Element>;
+  /**
+   * Options to pass to `renderToReadableStream`.
+   */
+  streamingOptions?: RenderToReadableStreamOptions;
+};
+
+/**
+ * frame-master-plugin-react-streaming - Frame-Master Plugin
+ *
+ * Description: Add your plugin description here
+ */
+export default function ReactStreamingPlugin(
+  options: ReactStreamingPluginOptions
+): FrameMasterPlugin {
+  const router = new Bun.FileSystemRouter({
+    dir: options.baseSrc,
+    style: "nextjs",
+    fileExtensions: [".tsx", ".jsx"],
+  });
+
+  return {
+    name,
+    version,
+    directives: [
+      {
+        name: "use-streaming",
+        regex:
+          /^(?:\s*(?:\/\/.*?\n|\s)*)?['"]use[-\s]streaming['"];?\s*(?:\/\/.*)?(?:\r?\n|$)/m,
+      },
+    ],
+    router: {
+      async request(master) {
+        if (master.isResponseSetted()) return;
+        const matched = router.match(master.URL.pathname);
+        if (!matched) return;
+        const _module = (await import(
+          matched.filePath + isDev() ? `?v=${Date.now()}` : ""
+        )) as { default?: () => JSX.Element | Promise<JSX.Element> };
+        if (!_module.default) return;
+
+        const pageElement = await _module.default();
+        master.setResponse(
+          await renderToReadableStream(
+            (await options.parseEntryPoint?.(pageElement, master)) ??
+              pageElement,
+            options.streamingOptions
+          ),
+          {
+            headers: {
+              "Content-Type": "text/html; charset=utf-8",
+            },
+          }
+        );
+      },
+    },
+
+    requirement: {
+      frameMasterVersion: "^3.0.0",
+      bunVersion: ">=1.2.0",
+    },
+  };
+}
